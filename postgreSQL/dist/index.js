c40e119934f4946b2b55f54dc32d32f8da450992
shubham-021 <shubham.arka@gmail.com>
2025-06-26 18:52:11 +0530

    postgreSQL row check

--- postgreSQL/dist/index.js
+++ postgreSQL/dist/index.js
@@ -11,38 +11,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const client = new pg_1.Client({
-    connectionString: "postgresql://neondb_owner:npg_MdrGIZ1DgmJ0@ep-jolly-scene-a1z8tit7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+    connectionString: "postgresql://newsCollector_owner:npg_MqsbxmVw70XF@ep-lingering-fog-a1r274g0-pooler.ap-southeast-1.aws.neon.tech/newsCollector?sslmode=require&channel_binding=require"
 });
 function connectDB() {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.connect();
     });
 }
-function createUserTable() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const result = yield client.query(`
-        CREATE TABLE users (
-        id SERIAL PRIMARY KEY ,
-        username VARCHAR(255) UNIQUE NOT NULL ,
-        email VARCHAR(255) NOT NULL ,
-        password VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-        );
-    `);
-        console.log(result);
-    });
-}
-function InsertDb(username, email, password) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const insertQuery = "INSERT INTO users (username , email , password) VALUES ($1 , $2 , $3)";
-        const values = [username, email, password];
-        const res = yield client.query(insertQuery, values);
-        console.log(res);
-    });
-}
 function showDB() {
     return __awaiter(this, void 0, void 0, function* () {
-        const showQuery = "SELECT * FROM users";
+        const showQuery = "SELECT * FROM headlines WHERE id=34";
         const res = yield client.query(showQuery);
         console.log(res);
     });
--- postgreSQL/src/index.ts
+++ postgreSQL/src/index.ts
@@ -2,7 +2,7 @@ import { Client } from "pg";
 
 const client = new Client({
 //  connectionString : "postgresql://userrname:password@host/dbName"
-    connectionString : "postgresql://neondb_owner:npg_MdrGIZ1DgmJ0@ep-jolly-scene-a1z8tit7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+    connectionString : "postgresql://newsCollector_owner:npg_MqsbxmVw70XF@ep-lingering-fog-a1r274g0-pooler.ap-southeast-1.aws.neon.tech/newsCollector?sslmode=require&channel_binding=require"
 })
 
 // or you can do this also:
@@ -22,32 +22,135 @@ async function connectDB(){
 }
 
 
-async function createUserTable(){
-    const result = await client.query(`
-        CREATE TABLE users (
-        id SERIAL PRIMARY KEY ,
-        username VARCHAR(255) UNIQUE NOT NULL ,
-        email VARCHAR(255) NOT NULL ,
-        password VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-        );
-    `)
+// async function createUserTable(){
+//     const result = await client.query(`
+//         CREATE TABLE users (
+//         id SERIAL PRIMARY KEY ,
+//         username VARCHAR(255) UNIQUE NOT NULL ,
+//         email VARCHAR(255) NOT NULL ,
+//         password VARCHAR(255) NOT NULL,
+//         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+//         );
+//     `)
 
-    console.log(result)
-}
+//     console.log(result)
+// }
 
-async function InsertDb(username : string , email : string , password : string){
-    const insertQuery = "INSERT INTO users (username , email , password) VALUES ($1 , $2 , $3)"
-    const values = [username , email , password]
-    const res = await client.query(insertQuery , values)
-    console.log(res)
-}
+// async function InsertDb(username : string , email : string , password : string){
+//     const insertQuery = "INSERT INTO users (username , email , password) VALUES ($1 , $2 , $3)"
+//     const values = [username , email , password]
+//     const res = await client.query(insertQuery , values)
+//     console.log(res)
+// }
 
 async function showDB(){
-    const showQuery = "SELECT * FROM users"
+    const showQuery = "SELECT * FROM headlines WHERE id=34"
     const res = await client.query(showQuery)
     console.log(res)
 }
+
+
+// res :
+ 
+// Result {
+//   command: 'SELECT',
+//   rowCount: 1,
+//   oid: null,
+//   rows: [
+//     {
+//       id: 34,
+//       title: 'U.S.-China tariff talks to continue on May 11, as Trump touts ’great progress made’',
+//       category: 'World Affairs',
+//       link: 'https://www.thehindu.com/news/international/us-china-tariff-talks-to-continue-on-may-11-as-trump-touts-great-progress-made/article69563306.ece',
+//       date: 2025-05-10T21:11:00.000Z,
+//       description: 'Donald Trump says “great progress” was being made in ongoing U.S.-China talks over tariffs menacing the global economy'
+//     }
+//   ],
+//   fields: [
+//     Field {
+//       name: 'id',
+//       tableID: 24590,
+//       columnID: 1,
+//       dataTypeID: 23,
+//       dataTypeSize: 4,
+//       dataTypeModifier: -1,
+//       format: 'text'
+//     },
+//     Field {
+//       name: 'title',
+//       tableID: 24590,
+//       columnID: 2,
+//       dataTypeID: 25,
+//       dataTypeSize: -1,
+//       dataTypeModifier: -1,
+//       format: 'text'
+//     },
+//     Field {
+//       name: 'category',
+//       tableID: 24590,
+//       columnID: 3,
+//       dataTypeID: 25,
+//       dataTypeSize: -1,
+//       dataTypeModifier: -1,
+//       format: 'text'
+//     },
+//     Field {
+//       name: 'link',
+//       tableID: 24590,
+//       columnID: 4,
+//       dataTypeID: 25,
+//       dataTypeSize: -1,
+//       dataTypeModifier: -1,
+//       format: 'text'
+//     },
+//     Field {
+//       name: 'date',
+//       tableID: 24590,
+//       columnID: 5,
+//       dataTypeID: 1114,
+//       dataTypeSize: 8,
+//       dataTypeModifier: 3,
+//       format: 'text'
+//     },
+//     Field {
+//       name: 'description',
+//       tableID: 24590,
+//       columnID: 6,
+//       dataTypeID: 25,
+//       dataTypeSize: -1,
+//       dataTypeModifier: -1,
+//       format: 'text'
+//     }
+//   ],
+//   _parsers: [
+//     [Function: parseInteger],
+//     [Function: noParse],
+//     [Function: noParse],
+//     [Function: noParse],
+//     [Function: parseDate],
+//     [Function: noParse]
+//   ],
+//   _types: TypeOverrides {
+//     _types: {
+//       getTypeParser: [Function: getTypeParser],
+//       setTypeParser: [Function: setTypeParser],
+//       arrayParser: [Object],
+//       builtins: [Object]
+//     },
+//     text: {},
+//     binary: {}
+//   },
+//   RowCtor: null,
+//   rowAsArray: false,
+//   _prebuiltEmptyResultObject: {
+//     id: null,
+//     title: null,
+//     category: null,
+//     link: null,
+//     date: null,
+//     description: null
+//   }
+// }
 
 connectDB();
 showDB()
